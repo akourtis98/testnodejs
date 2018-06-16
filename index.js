22f2715ced1ee5b34555140590f6329a9257b1ac
alex <akourtisdev@gmail.com>
2018-06-16 08:14:41 +0300

    .

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer(function(req, res) {
-
     const parsedUrl = url.parse(req.url, true);
-
     const path = parsedUrl.pathname;
-
-    const trimmedPath = path.replace(/\/+|\/+$/g, '');
-
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const queryStringObject = parsedUrl.query;
+    const method = req.method.toLocaleLowerCase();
     res.end("hello mate");
+
+    console.log("req recieved on path: " + trimmedPath + " with method: " + method + " and with these query string parameters: ", queryStringObject);
 });
 
 server.listen(3000, function() {
